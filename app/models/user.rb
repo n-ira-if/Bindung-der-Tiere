b59d267c87cccc1a591a016c6688f7bbdb1d70d3
n-ira-if <ibnoaria@yahoo.co.jp>
2024-12-05 17:25:49 +0900

    ユーザー登録完了
    ユーザー登録できることを確認
    一部ビューファイル修正
    ランダムメッセージ機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :region
+
+
+  validates :region_id, presence: true, numericality: { only_integer: true, other_than: 1 }
+
 end
